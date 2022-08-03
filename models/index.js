3beb909f76545b65a7691ab5b3e1288cd0db7b82
MosNes <7hekarl@gmail.com>
2022-08-03 17:55:37 -0400

    updated models/index.js with imports and exports

--- models/index.js
+++ models/index.js
@@ -1,1 +1,14 @@
 //index of models
+//collects all models into single file to be easily imported into other files
+
+const User = require('./User');
+const Nest = require('./Nest');
+const Task = require('./Task');
+const Assignment = require('./Assignment');
+
+//define assoctiations between models
+
+
+
+//exports models
+module.exports = { User, Nest, Task, Assignment };
