f540301bcc367ed17bf26c67518c46c2278b3053
MosNes <7hekarl@gmail.com>
2022-08-05 19:00:51 -0400

    added login route that accepts username or email address, added logout route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,6 +2,7 @@
 
 //------------DEPENDENCIES AND GLOBAL VARIABLES------------------------------
 const router = require("express").Router();
+const e = require("express");
 const { User, Nest } = require("../../models");
 
 //Get All Users
@@ -131,6 +132,68 @@ router.delete('/:id', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+//login route
+// accepts username or email?
+router.post('/login', (req, res) => {
+
+    //expects {identifier: 'username or email address'}
+
+    //regex to check if input string is an email address
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/gi;
+
+    //if identifier is an email address, use object with the email property for query
+    if ( emailRegex.test(req.body.identifier) ) {
+        var whereCriteriaObj = {
+            email: req.body.identifier
+        }
+    //else, use object with username property for query
+    } else {
+        var whereCriteriaObj = {
+            username: req.body.identifier
+        }
+    }
+
+    User.findOne({
+        where: whereCriteriaObj
+    })
+        .then(async dbUserData => {
+            if (!dbUserData) {
+                res.status(400).json({ message: 'No user found with that email address or username!' });
+                return;
+            }
+
+            //load hash from your password DB
+            const validPassword = await dbUserData.checkPassword(req.body.password);
+
+            if (!validPassword) {
+                res.status(400).json({ message: "Incorrect Password!" });
+                return;
+            }
+
+            //creates a session
+            req.session.save(() => {
+                //declare session variables
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json({ user: dbUserData, message: 'You are now logged in!' });
+            });
+
+        });
+});
+
+//logout route
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy( () => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 });
 
 module.exports = router;
