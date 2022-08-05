7d23d63274657e11ba458e1740e1bca99fc6dfc8
MosNes <7hekarl@gmail.com>
2022-08-04 20:25:26 -0400

    added skeletons for other routes

---
+++ controllers/api/assignment-routes.js
@@ -0,0 +1,15 @@
+//Assignment Routes
+
+//get assignment by ID
+
+//get Assignments by Nest ID
+
+//Get Assignments by User ID
+
+//create Assignment
+
+//update assignment
+
+//mark assignment as complete
+
+//Delete Assignment
---
+++ controllers/api/nest-routes.js
@@ -0,0 +1,8 @@
+//Nest API CRUD Routes
+
+
+
+//Add User to Nest
+
+
+//Get Users by Nest ID
