82398b785c9730f93e73ea4ec0226e2c598e9b6b
MosNes <7hekarl@gmail.com>
2022-08-06 10:48:48 -0400

    added and tested all Nest routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,10 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
+const nestRoutes = require('./nest-routes');
 
 //use userRoutes for any request starting with /users/
 router.use('/users', userRoutes);
+router.use('/nests', nestRoutes);
 
 module.exports = router;
--- controllers/api/nest-routes.js
+++ controllers/api/nest-routes.js
@@ -1,8 +1,145 @@
 //Nest API CRUD Routes
+const router = require('express').Router();
+const { User, Nest, Task } = require('../../models');
 
+//Get all Nests
+router.get('/', (req, res) => {
+    Nest.findAll({
+        //do not return personal info on findAll
+        attributes: { exclude: ['street', 'city', 'state', 'zip', 'share_id']}
+    })
+    .then(dbNestData => res.json(dbNestData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
+//Get Nest info by ID - REQUIRES AUTH AND NEST MEMBERSHIP
+router.get('/:id', (req, res) => {
+    Nest.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'nest_name',
+            'street',
+            'city',
+            'state',
+            'zip',
+            'share_id'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['id', 'username', 'email', 'first_name','last_name', 'role']
+            },
+            {
+                model: Task,
+                attributes: ['id', 'task_name','task_description']
+            }
+        ]
+    })
+    .then(dbNestData => res.json(dbNestData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//create Nest
+router.post('/', (req, res) => {
+    //expects {nest_name: 'name', street: 'street address', city: 'city', state: 'NC', zip: 'zip code'}
+    Nest.create({
+        nest_name: req.body.nest_name,
+        street: req.body.street,
+        city: req.body.city,
+        state: req.body.state,
+        zip: req.body.zip
+    })
+    .then(dbNestData => res.json(dbNestData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 //Add User to Nest
+router.put('/add-user', (req, res)=> {
+    //expects {nest_id: 'nest id', user_id: 'user's id'}
+    User.update(
+        {
+            nest_id: req.body.nest_id
+        },
+        {
+            where: {
+                id: req.body.user_id
+            }
+        }
+    )
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user found with that ID' })
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//update Nest info
+router.put('/:id', (req, res) => {
+    //expects {nest_name: 'name', street: 'street address', city: 'city', state: 'state', zip: 'zip code'}
+    Nest.update(
+        {
+            nest_name: req.body.nest_name,
+            street: req.body.street,
+            city: req.body.city,
+            state: req.body.state,
+            zip: req.body.zip
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+        .then(dbNestData => {
+            if (!dbNestData) {
+                res.status(400).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbNestData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+//delete Nest
+router.delete('/:id', (req, res) => {
+    Nest.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbNestData => {
+            if (!dbNestData) {
+                res.status(404).json({ message: 'No post found with this id' });
+                return;
+            }
+            res.json(dbNestData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 
-//Get Users by Nest ID
+module.exports = router;
