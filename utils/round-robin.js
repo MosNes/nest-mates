31b34505057dee3f7b443522cc6dde3262253bba
MosNes <7hekarl@gmail.com>
2022-08-06 15:26:48 -0400

    added and tested roundRobin() function

---
+++ utils/round-robin.js
@@ -0,0 +1,23 @@
+const { SequentialRoundRobin } = require("round-robin-js");
+
+const roundRobin = (nestMateArr, taskArr, iterations) => {
+  //create a looping table of the users in a nest
+  const nestMatesTable = new SequentialRoundRobin(nestMateArr);
+
+  //create a looping table of tasks (daily, weekly or monthly)
+  const taskTable = new SequentialRoundRobin(taskArr);
+
+  //empty array to hold the returned pairings
+  const assignmentArr = [];
+
+  //for the number of iterations specified (30 days, 3 weeks, etc) do this
+  for (let i = 0; i < iterations; i++) {
+    //pair the next user with the next task
+    assignmentArr.push([nestMatesTable.next().value, taskTable.next().value]);
+  }
+
+  //returns array of pairings
+  return assignmentArr;
+};
+
+module.exports = roundRobin;
