73386a98481e8e3fd91bcf62320bf43cad17cb01
MosNes <7hekarl@gmail.com>
2022-08-10 18:54:23 -0400

    added route to home-routes.js to redirect a logged in user to their mynest page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,68 @@
 const router = require('express').Router();
+const {Task, Assignment, User} = require('../models');
 
-router.get("/", (req, res) => { 
+//display homepage
+//if user is logged in, redirect to myNest page
+router.get("/", (req, res) => {
+    if (req.session.loggedIn){
+        res.redirect('/mynest');
+        return;
+    }
+
     res.render('homepage')
+});
+
+router.get('/mynest', (req, res) => {
+
+        Assignment.findAll({
+            where: {
+                nest_id: req.session.nest_id
+            },
+            attributes: [
+                'id',
+                'date',
+                'completion_date',
+                'status',
+                'note',
+                'task_id',
+                'user_id',
+                'nest_id'
+            ],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username', 'email', 'first_name','last_name']
+                },
+                {
+                    model: Task,
+                    attributes: ['task_name', 'task_description', 'recurs']
+                }
+            ]
+        })
+            .then(dbAssignmentData => {
+                if (!dbAssignmentData) {
+                    res.status(404).json({ message: 'No assignment found with this id' });
+                    return;
+                }
+
+                //serialize the data into an array of objects
+                const assignments = dbAssignmentData.map( assignment => assignment.get({ plain: true }));
+
+                //pass array of formatted assignment objects to the mynest template
+                //session.loggedIn is passed to the template so the template can render
+                //differently depending on whether a user is logged in
+                res.render('mynest', {
+                    assignments,
+                    loggedIn: req.session.loggedIn
+                })
+
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
     });
+    res.render('mynest');
 
 
 module.exports= router;
