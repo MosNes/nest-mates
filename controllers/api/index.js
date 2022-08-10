5b7549b2340368032049c24d093a71447ab92f8c
MosNes <7hekarl@gmail.com>
2022-08-10 18:26:47 -0400

    updated controllers/api/index.js with new assignment routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,13 +5,13 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const nestRoutes = require('./nest-routes');
-
-const tasksRoutes = require('./task-routes.js');
+const taskRoutes = require('./task-routes.js');
+const assignmentRoutes = require('./assignment-routes');
 
 //use userRoutes for any request starting with /users/
 router.use('/users', userRoutes);
 router.use('/nests', nestRoutes);
-
-router.use('/tasks', tasksRoutes);
+router.user('assignments', assignmentRoutes);
+router.use('/tasks', taskRoutes);
 
 module.exports = router;
