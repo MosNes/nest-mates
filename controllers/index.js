7e54e38753bfb7e70ae47ff4298afd60841b907f
cmon-theres-gotta-be-a-name-available <ben.ridgeway9@gmail.com>
2022-08-12 15:34:20 -0400

    trying to fix issue with signup page not displaying

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@
 const router = require('express').Router();
 const apiRoutes = (require('./api'));
 const signroutes = (require('./home-routes'));
+const logroutes = (require('./home-routes'))
 //use apiRoutes for any request starting with /api/
 router.use('/api',apiRoutes);
 router.use("/", signroutes);
+router.use("/login", logroutes);
 router.use( (req, res) => {
     res.status(404).end();
 });
